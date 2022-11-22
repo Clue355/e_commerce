223fd370212d8b36f17086bb17701c0dabf5692f
Your Name <you@example.com>
2022-11-21 20:17:01 -0600

    fix: any typescript error

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
-export default function Product({ id, title, desc, price, image }: any) {
+interface Props {
+    id: string;
+    title: string;
+    desc: string;
+    price: number;
+    image: string;
+}
+
+export default function Product({ id, title, desc, price, image }: Props) {
     return (
         <div key={id} className="mr-20">
             <Image src={image} width={300} height={300} alt="clothing" />
